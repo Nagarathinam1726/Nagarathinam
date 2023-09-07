d1c9e3536ce9605fc3242cb8f28ec800d5150522
Nagarathinam1726 <144340920+Nagarathinam1726@users.noreply.github.com>
2023-09-07 16:55:31 +0530

    Unit 1

---
+++ main (2).py
@@ -0,0 +1,14 @@
+def isLeapYear (year):
+  if (year % 40 and year % 100 != 0) or year % 400 = 0:
+    return True
+  else:
+    return False
+
+year = int(input("Enter a year : "))
+
+if isLeapYear (year):
+  print("{} is a leap year.'
+  .format(year))
+else:
+  print('{} is not leap year.'.
+        .format(year))
